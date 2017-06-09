9de493f816d7762cd4eaade524b0edf6046c6543
fboliveira <fboliveira@hugo>
2017-06-09 13:20:32 -0300

    Code template

---
+++ modelo.c
@@ -0,0 +1,19 @@
+/*
+* CSI030-2017-01 - Programacao de Computadores I
+* Nome........: (seu nome completo)
+* Matricula...: 99.9.9999
+* Curso.......: (nome do curso)
+* Exercicio...: pagina X - exercicio Y / atividade pratica Z
+*/
+
+/* Bibliotecas */
+#include <stdio.h>
+
+/* Funcao principal */
+int main(int argc, char const *argv[]) {
+
+  /* Comente o seu codigo */
+
+  return 0;
+
+}
