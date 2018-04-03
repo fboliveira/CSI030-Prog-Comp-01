b65541253ae09e2affef8ec754c57581f40705bf
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-04-03 15:06:49 -0300

    Class Apr 3rd, 2018. C: I/O

---
+++ Codes/001-introducao/02-dias.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+  int idade;
+  int dias;
+
+  // Ler a idade
+  printf("Digite a sua idade: ");
+  scanf("%d", &idade);
+
+  // Calcular a quantidade de dias
+  dias = idade * 365;
+
+  // Imprimir o resultado
+  printf("A quantidade em dias e: %d\n\n", dias);
+
+
+
+
+
+
+
+  return 0;
+}
