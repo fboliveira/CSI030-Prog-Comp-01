bf633c75d79bc985e2613f1570c7d2096426b8e4
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-04-05 20:30:01 -0300

    Class Apr 5th, 2017. C: expressions.

--- Codes/001-introducao/02-dias.c
+++ Codes/001-introducao/02-dias.c
@@ -4,6 +4,7 @@ int main(int argc, char const *argv[]) {
 
   int idade;
   int dias;
+  int horas;
 
   // Ler a idade
   printf("Digite a sua idade: ");
@@ -12,8 +13,16 @@ int main(int argc, char const *argv[]) {
   // Calcular a quantidade de dias
   dias = idade * 365;
 
+  // Calcular a quantidade em horas
+  horas = dias * 24;
+
   // Imprimir o resultado
-  printf("A quantidade em dias e: %d\n\n", dias);
+  printf("A quantidade em dias e: %d e em horas e %d\n\n", dias, horas);
+  //printf("A quantidade em horas e: %d\n\n", horas);
+
+
+
+
 
 
 
---
+++ Codes/001-introducao/03-expressoes.c
@@ -0,0 +1,38 @@
+
+
+
+
+#include <stdio.h>
+
+int main() {
+
+  int x = 5;
+  float y = 2;
+  int z = 3;
+  int j = 8;
+
+  float w = x / y;
+  float k = z / w;
+
+  float r = x / 4.0;
+  float b = (float)j / x;
+
+  x = x + 1;
+  x++;
+
+  x = x - 1;
+  x--;
+
+  printf("A divisao de %d por %.2f e igual a %.2f\n", x, y, w);
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
