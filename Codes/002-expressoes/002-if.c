5322672ef06e7f3249651d888a1715cce524ddcb
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-04-12 18:13:06 -0300

    Class Apr 12th, 2018. C: expressions and exercises;

--- Codes/002-expressoes/002-if.c
+++ Codes/002-expressoes/002-if.c
@@ -12,10 +12,12 @@ int main(int argc, char const *argv[]) {
   printf("Informe o valor pago:" );
   scanf("%d", &pago);
 
-  if (pago >= conta) {
-      troco = pago - conta;
-      printf("Conta paga com sucesso!\n");
-      printf("O troco e de: %d\n", troco);
+  if (conta > 0 && pago >= conta) {
+        troco = pago - conta;
+        printf("Conta paga com sucesso!\n");
+        if (troco > 0) {
+          printf("O troco e de: %d\n", troco);
+        }
   } else {
       printf("O valor pago e insuficiente!\n");
   }
