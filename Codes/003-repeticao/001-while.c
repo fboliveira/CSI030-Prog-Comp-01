429a524b54893bace013fd4bacc787c962228f14
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-04-20 10:21:20 -0300

    Class Apr 19th, 2017. C: while.

--- Codes/003-repeticao/001-while.c
+++ Codes/003-repeticao/001-while.c
@@ -4,12 +4,19 @@ int main(int argc, char const *argv[]) {
 
   int x;
   int n;
+  int aux;
 
-  printf("Informe o valor minimo: ");
+  printf("Informe o primeiro valor: ");
   scanf("%d", &x);
 
-  printf("Informe o numero maximo: ");
+  printf("Informe o segundo valor: ");
   scanf("%d", &n);
+
+  if (x > n) {
+    aux = x;
+    x = n;
+    n = aux;
+  }
 
   while( x <= n ) {
     printf("%d\n", x);
