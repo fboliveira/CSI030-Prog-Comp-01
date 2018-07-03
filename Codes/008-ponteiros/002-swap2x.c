952f70dc7cfdfa85b03ec44b8d97d146e30572b7
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-07-03 15:49:40 -0300

    Class Jul 3rd, 2018. C: pointers.

---
+++ Codes/008-ponteiros/002-swap2x.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void swap2x(int *a, int *b) {
+
+  int temp;
+
+  // Troca
+  temp = *a;
+  *a = *b;
+  *b = temp;
+
+  // Duplica
+  *a = (*a) * 2;
+  *b *= (*b) * 2;
+
+}
+
+int main(int argc, char const *argv[]) {
+
+  int x = 5, y = 3;
+  swap2x(&x, &y);
+
+  printf("X = %d\nY = %d", x, y);
+
+
+
+
+
+
+
+
+  return 0;
+}
