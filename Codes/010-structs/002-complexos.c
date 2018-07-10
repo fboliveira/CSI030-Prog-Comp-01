e7bf18f042196dd0f079d644bf8c8cdd9c43b9f1
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-07-10 18:24:35 -0300

    Class Jul 10th, 2018. C: din. aloc. / structs.

---
+++ Codes/010-structs/002-complexos.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+typedef struct regComplexo {
+  float real;
+  float imag;
+} Complexo;
+
+void ler(Complexo *n) {
+
+  printf("Parte real: ");
+  scanf("%f", n->real);
+  printf("Parte imaginaria: ");
+  scanf("%f", (*n).imag);
+
+}
+
+int main(int argc, char const *argv[]) {
+
+  Complexo n1, n2, soma;
+  Complexo num[2];
+  Complexo matriz[5][5];
+
+  Complexo *p;
+  p = (Complexo *) malloc( 5 * sizeof(Complexo) );
+
+  ler(&n1);
+
+  // Ler os numeros
+  printf("Primeiro numero: \n");
+  printf("Parte real: ");
+  scanf("%f", &n1.real);
+  printf("Parte imaginaria: ");
+  scanf("%f", &n1.imag);
+
+  printf("Segundo numero: \n");
+  printf("Parte real: ");
+  scanf("%f", &n2.real);
+  printf("Parte imaginaria: ");
+  scanf("%f", &n2.imag);
+
+  // Operar n1 + n2
+  soma.real = n1.real + n2.real;
+  soma.imag = n1.imag + n2.imag;
+
+  printf("A soma: %f + ( %f )i\n", soma.real, soma.imag);
+
+  return 0;
+}
