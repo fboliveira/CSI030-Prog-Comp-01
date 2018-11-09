1be3ae35a9c12decbd2fbb0c56481894dc4100f3
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-11-09 18:27:40 -0200

    Class Nov 9th, 2018. C: strings.

--- Codes/005-vetores/05-strings-nome.c
+++ Codes/005-vetores/05-strings-nome.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 
+int substituir(char nome[]) {
+
+  int i, trocas = 0;
+
+  for(i = 0; nome[i] != '\0'; i++) {
+
+    if ( nome[i] == 'a' || nome[i] == 'A' ) {
+      nome[i] = '4';
+      trocas++;
+    } else if ( nome[i] == 'e' || nome[i] == 'E' ) {
+      nome[i] = 's';
+      trocas++;
+    }
+
+  }
+
+  return trocas;
+
+}
+
+void soletrar(char nome[]) {
+
+  int i;
+
+  for(i = 0; nome[i] != '\0'; i++) {
+    printf("%c", nome[i]);
+
+    if( nome[i+1] != '\0' ) {
+      printf(" - ");
+    }
+
+  }
+
+  printf("\n");
+
+}
+
 
 int main(int argc, char const *argv[]) {
 
   char nome[51];
-  int i, vogais = 0;
+  int i, vogais = 0, trocas;
 
   printf("Digite o seu nome: ");
   scanf("%[^\n]s", nome);
@@ -29,8 +66,13 @@ int main(int argc, char const *argv[]) {
 
   printf("Vogais = %d\n", vogais);
 
+  // Substituicao
+  trocas = substituir(nome);
+  printf("Nome apos substituicao: %s\n", nome);
+  printf("Trocas = %d\n", trocas);
 
-
+  // soletrar
+  soletrar(nome);
 
 
   return 0;
