ad8e4dedc2ee2dc434602f6cd3994f8d1fdfccd6
Fernando B Oliveira <fboliveira25@gmail.com>
2020-05-07 15:40:31 -0300

    Grupo de estudo - variáveis

---
+++ Codes/grupo-estudo-programacao/01-revisao-variaveis/01-ola-mundo.c
@@ -0,0 +1,17 @@
+// Definicao das bibliotecas
+#include <stdio.h>
+
+// Funcao principal do programa
+int main() {
+
+    // Saida - comando em tela
+    printf("Ola, mundo!\n");
+    printf("Grupo de estudo");
+
+}
+// Resultado:
+
+// ./01-ola-mundo.c.run 
+// Ola, mundo!
+// Grupo de estudo
+
---
+++ Codes/grupo-estudo-programacao/01-revisao-variaveis/02-variaveis.c
@@ -0,0 +1,35 @@
+// Definicao das bibliotecas
+#include <stdio.h>
+
+// Funcao principal - daqui o computador comeca a executar o programa:
+int main() {
+
+    // tipo nome
+    // Idade da pessoa
+    int idade; // lixo na memoria
+    // Idade em meses
+    int meses;
+    // Altura
+    float altura;
+    // Primeira letra do nome
+    char letra;
+
+    // Entrada pelo teclado:
+    scanf("%d", &idade);
+
+    // Calcula a idade em meses
+    meses = idade * 12;
+
+    // A idade da pessoa em meses:
+    printf("Idade em meses: %d\n", meses);
+
+    // Calcular a idade:
+    // - em dias;
+    // - em horas;
+    // - em segundos;
+
+    // Rever: (Moodle)
+    // - notas de aula 01 e 02 
+    // - apostila - Capítulo 1 (expressões lógicas e relacionais)
+
+}
