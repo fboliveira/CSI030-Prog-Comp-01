cca8292db9c22b378c1a18b48ad53c7d4a1971e5
Fernando B Oliveira <fboliveira25@gmail.com>
2022-09-13 22:06:03 -0300

    feat(Subroutines): :sparkles: Add soma intervalo e equacao segundo grau

--- Codes/004-functions/03-somaintervalo.c
+++ Codes/004-functions/03-somaintervalo.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
 
+int ehimpar(int);
+int somaintervalo(int, int);
+int contaimpar(int n1, int n2);
+
+
+int main(int argc, char const *argv[]) {
+
+    int n1, n2;
+    printf("Digite dois numeros: ");
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+
+    // soma = somaintervalo(n1, n2);
+
+    printf("Soma = %d\n", somaintervalo(n1, n2));
+    printf("Impares = %d\n", contaimpar(n1, n2));
+
+    return 0;
+}
+
+int ehimpar(int n) {
+    return ( n % 2 != 0 ); 
+}
+
+int contaimpar(int n1, int n2) {
+    int i;
+    int impar = 0;
+    int inicio;
+    int fim;
+
+    if (n1 < n2) {
+        inicio = n1;
+        fim = n2;
+    } else {
+        inicio = n2;
+        fim = n1;
+    }
+
+    for(i = inicio; i <= fim; i++) {
+        if ( ehimpar(i) ) {
+            impar++;
+        }
+    }    
+
+    return impar;
+
+}
+
+
+
 int somaintervalo(int n1, int n2) {
 
     int soma = 0;
@@ -17,19 +67,4 @@ int somaintervalo(int n1, int n2) {
 
     return soma;
 
-}
-
-
-int main(int argc, char const *argv[]) {
-
-    int n1, n2;
-    printf("Digite dois numeros: ");
-    scanf("%d", &n1);
-    scanf("%d", &n2);
-
-    // soma = somaintervalo(n1, n2);
-
-    printf("Soma = %d\n", somaintervalo(n1, n2));
-
-    return 0;
 }
