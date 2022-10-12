1172911d061ab2157c545a34318cba10371070fd
Fernando B Oliveira <fboliveira25@gmail.com>
2022-10-11 22:10:03 -0300

    Pointers and dynamic allocation

---
+++ Codes/009-dynamic-allocation/exemplos/02-alocdim-vet-calloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM 20
+
+int main(int argc, char const *argv[]) {
+
+	int *p, i;
+	p = (int *) calloc(TAM,sizeof(int));
+
+	if (!p)
+		printf("Memoria insuficiente\n");
+	else {
+		for(i=0;i<TAM;i++)
+			*(p+i)=i;
+
+		for(i=0;i<TAM;i++)
+			printf("%d \t",p[i]);
+
+		printf("\n\n\n");
+
+		free(p);
+	}
+}
