15dbf99cf6af4d48f25354da53e194c123a5efdf
Fernando B Oliveira <fboliveira25@gmail.com>
2024-07-12 21:23:52 -0300

    Added first code

---
+++ Codes/000-intro/01-hello.c
@@ -0,0 +1,18 @@
+// Comentario em linha
+/*
+Comentario 
+em 
+bloco.
+*/
+// Bibliotecas
+#include <stdio.h>
+
+void imprimir(int x) {
+    printf("Valor = %d\n", x);
+}
+
+int main() {
+
+    printf("Ola, mundo!");
+
+}
