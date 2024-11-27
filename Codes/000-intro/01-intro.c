894db819df93a647a9683c30b023491b582d8143
Fernando B Oliveira <fboliveira25@gmail.com>
2024-11-27 17:16:05 -0300

    First class

---
+++ Codes/000-intro/01-intro.c
@@ -0,0 +1,20 @@
+// Declarar/incluir as bibliotecas
+#include <stdio.h>
+
+// Função principal
+// <retorno> <nome> (<parametros?>)
+int main() {
+
+    // Declarar variáveis:
+    // <tipo> <nome>
+    int x;
+    // Atribuir um valor
+    // <nome> = <valor>
+    x = 27; // 27/11
+
+    // Instruções - comandos
+    printf("Ola,\tmundo! Hoje e dia: %d \n\n\n", x);
+
+    return 0;
+
+}
