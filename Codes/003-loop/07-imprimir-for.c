09e13d7378442331402ed2d28286a5b173938068
Fernando B Oliveira <fboliveira25@gmail.com>
2025-01-22 19:23:54 -0300

    Added for loops

---
+++ Codes/003-loop/07-imprimir-for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+    // int x;
+
+    for(int x = 1; x <= 10; x++) {
+        printf("%d\n", x);
+    }
+
+    printf("Fim da impressao.\n");
+    return 0;
+}
