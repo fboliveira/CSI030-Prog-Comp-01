ce4378a47b4c62c520e3425edc8da4d5649bbbfa
Fernando B Oliveira <fboliveira25@gmail.com>
2025-02-10 19:44:16 -0300

    Added subroutines: exercises

---
+++ Codes/004-subroutines/04-fatorial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int fatorial(int n) {
+
+    int i, fat;
+    fat = 1;
+
+    // for(i = 1; i <= n; i++) {
+    for(i = n; i >= 1; i--) {
+        fat *= i;
+    }
+
+    return fat;
+
+}
+
+int main(int argc, char const *argv[]) {
+
+    int n, fat;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    fat = fatorial(n);
+
+    printf("Fatorial = %d\n", fat);
+    
+    return 0;
+
+}
